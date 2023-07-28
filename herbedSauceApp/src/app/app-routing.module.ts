d48066defbba4e447421f6d2dd6aed3ca917745a
graan501 <danielle.vangraan@kpn.com>
2023-07-28 20:23:24 +0200

    update to angular material

--- herbedSauceApp/src/app/app-routing.module.ts
+++ herbedSauceApp/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, MatSlideToggleModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
